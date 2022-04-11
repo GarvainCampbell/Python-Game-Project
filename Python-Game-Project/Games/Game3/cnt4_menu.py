f2d9114f744305afdf934dd38cd5fa3d1b33e99a
davia-g <daviag300@gmail.com>
2022-04-11 13:13:47 -0500

    changed game to connect 4. have not figured out menu and play again screens.

--- Python-Game-Project/Games/Game3/cnt4_menu.py
+++ Python-Game-Project/Games/Game3/cnt4_menu.py
@@ -38,7 +38,7 @@ class Button():
 SCREEN = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Menu")
 
-BG = pygame.image.load("assets/gradient.png")
+BG = pygame.image.load("assets/bg.jpeg")
 
 
 def get_font(size):  # Returns Press-Start-2P in the desired size
@@ -56,7 +56,7 @@ def play():
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
 
         PLAY_BACK = Button(image=None, pos=(640, 460),
-                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="Yellow")
 
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(SCREEN)
@@ -77,13 +77,13 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render('CONNECT FOUR', True, "#000000")
+        MENU_TEXT = get_font(100).render('CONNECT FOUR', True, "white")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 350),
-                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                             text_input="PLAY", font=get_font(75), base_color="White", hovering_color="#ffff62")
         QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 500),
-                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                             text_input="QUIT", font=get_font(75), base_color="White", hovering_color="#ff3232")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
